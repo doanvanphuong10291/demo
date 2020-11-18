ca6594d95ff0b9eef4eecd6bbf0b6f12dd37046c
doanvanphuong10291 <68044045+doanvanphuong10291@users.noreply.github.com>
2020-11-18 17:47:07 +0700

    update 2345

--- app/src/main/java/tdc/example/demo/MainActivity.java
+++ app/src/main/java/tdc/example/demo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get all view from layout
+        // Get all view from layout 2345
     }
 }
